700d6dc0bccbb64586a270ed1e1911aed4d94150
StevenLiu <stevenliu986@gmail.com>
2026-06-29 10:56:04 +1000

    文件操作及示例

--- 09-文件操作.py
+++ 09-文件操作.py
@@ -20,13 +20,31 @@ print(type(file))
 # print(line3)
 
 # 循环读取文件内容
-for line in file:
-    print(line)
+# for line in file:
+#     print(line)
 
 # 关闭文件
 file.close()
 
 # with open 操作文件，会自动关闭文件
-with open('测试.txt', 'r', encoding='utf-8') as f:
-    for line in f:
-        print(line)
+# with open('测试.txt', 'r', encoding='utf-8') as f:
+#     for line in f:
+#         print(line)
+
+
+# 文件写入 - file.write(str)
+file = open('测试.txt', 'w', encoding='utf-8')
+# 写入内容
+file.write('CBA I am coming')
+# 刷新文件
+file.flush()
+file.close()
+
+# 文件追加
+# 打开一个不存在的文件（会新建它并写入内容）
+file1 = open('test.txt', 'a', encoding='utf-8')
+file1.write('Hello CBA, I am coming. ')
+file1.flush()
+file1.close()
+file2 = open('test.txt', 'r', encoding='utf-8')
+print(file2.read())
